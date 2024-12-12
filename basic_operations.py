5055af897bbc5700b996de5900cdb74e2bf3b3f3
Mustapha Osman <mustaff2277@gnail.com>
2024-12-12 16:51:38 +0000

    made some changes

--- basic_operations.py
+++ basic_operations.py
@@ -5,6 +5,6 @@ sum = number1 + number2
 difference = number1 - number2
 product = number1 * number2
 
-print(f"Addition of {number1} and {number2} = {sum}")
-print(f"Subtraction of {number1} and {number2} = {difference}")
-print(f"Multiplication of {number1} and {number2} = {product}")
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
