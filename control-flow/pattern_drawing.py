aa2c5d01f7fb4536498dd6c3a727804c6db48f6a
Mustapha Osman <mustaff2277@gnail.com>
2024-12-22 12:19:30 +0000

    Drawing Patterns with Nested Loops

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,15 +1,10 @@
 size = int(input("Enter the size of the pattern: "))
 
-i = size
-count = 0
+row = 0
 
-while i > 0:
-    print("*", end="")
-    i -= 1
-    for _ in range(1):
-        if count == size - 1:
-            break
-        elif i == 0:
-            i = size
-            count += 1
-            print("\n")
+while row < size:
+   
+    for _ in range(size):
+        print("*", end="") 
+    print()  
+    row += 1
