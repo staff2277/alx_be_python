bc81ad4f13fd2bd68e59ba6d0ac847311c1afc72
Mustapha Osman <wnfynn@gmail.com>
2025-01-11 15:23:42 +0000

    created a shopping cart

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,31 +1,39 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
 
+def main():
+    shopping_list = []
+    
+    while True:
+        display_menu()
+        prompt = int(input("Enter a number: "))
 
+        if prompt == 1:
+            item = input("Add an item: ")
+            shopping_list.append(item)
 
-def main ():
-     shopping_list = []
-     while True:
-          print("Shopping List Manager")
-          print("1. Add Item")
-          print("2. Remove Item")
-          print("3. View List")
-          print("4. Exit")
+        elif prompt == 2:
+            rm = input("Remove an item: ")
+            if rm in shopping_list:
+                shopping_list.remove(rm)
+            else:
+                print(f"{rm} not found in the shopping list.")
 
-          prompt = int(input("Enter a number: "))
+        elif prompt == 3:
+            print("\nShopping List:")
+            list(map(lambda value: print(value), shopping_list))
 
-          item = str(input("Add an item: ")) if prompt == 1 else None
-          shopping_list.append(item) if prompt == 1 else None
+        elif prompt == 4:
+            print("The program has ended.")
+            break
 
-          rm = str(input("Remove an item: ")) if prompt == 2 else None
-          shopping_list.remove(rm) if prompt == 2 else None
-
-          print() if prompt == 3 else None
-          print("Shopping List") if prompt == 3 else None
-          list(map(lambda value: print(value), shopping_list)) if prompt == 3 else None
-
-          print()
-
-          if prompt == 4:
-               print("The program has ended")
-               break
+        else:
+            print("Invalid option. Please choose a valid option.")
+        
+        print()
 
 main()
