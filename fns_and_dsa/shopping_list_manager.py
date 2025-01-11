44be5cc366cd6c01ed8165ce7b083257ca90abda
Mustapha Osman <wnfynn@gmail.com>
2025-01-11 15:24:57 +0000

    shopping cart

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,24 +10,24 @@ def main():
     
     while True:
         display_menu()
-        prompt = int(input("Enter a number: "))
+        choice = int(input("Enter a number: "))
 
-        if prompt == 1:
+        if choice == 1:
             item = input("Add an item: ")
             shopping_list.append(item)
 
-        elif prompt == 2:
+        elif choice == 2:
             rm = input("Remove an item: ")
             if rm in shopping_list:
                 shopping_list.remove(rm)
             else:
                 print(f"{rm} not found in the shopping list.")
 
-        elif prompt == 3:
+        elif choice == 3:
             print("\nShopping List:")
             list(map(lambda value: print(value), shopping_list))
 
-        elif prompt == 4:
+        elif choice == 4:
             print("The program has ended.")
             break
 
