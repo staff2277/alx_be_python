bc4f38fb419c245a9bcc17e714e14a44f9043e2c
Mustapha Osman <wnfynn@gmail.com>
2025-01-19 22:45:54 +0000

    q2

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,13 +9,14 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
 
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(5, 9), -4)
+        
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
 
-    def test_subtract(self):
-        self.assertEqual(self.calc.subtract(5, 3), 2)
-        self.assertEqual(self.calc.subtract(5, 9), -4)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(4, 2), 2)
