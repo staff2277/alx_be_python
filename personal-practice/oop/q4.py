71d0e757a095e1cc5c408e6be38143430fd5eb3d
Mustapha Osman <wnfynn@gmail.com>
2025-01-23 21:05:09 +0000

    done with question 4

---
+++ personal-practice/oop/q4.py
@@ -0,0 +1,13 @@
+class calculator:
+    """ def __init__(self, num1, num2):
+        self.num2 = num1
+        self.num2 = num2 """
+    
+
+    @staticmethod
+    def add(num1, num2):
+        return num1 + num2
+
+
+
+print(calculator.add(5, 4))
